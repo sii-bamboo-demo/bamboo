19c5fe2d9210ace8706f93322607acca945e32c5
Mateusz Tadla <mtadla@sii.pl>
2020-03-31 01:32:02 +0200

    add demo

---
+++ AutomationTestSiiFramework/Base/DemoTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Allure.Core;
+using NUnit.Framework;
+
+namespace BambooDemo.Base
+{
+    [AllureNUnit]
+    [TestFixture]
+    public class DemoTests 
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            1.Should().Be(1);
+        }
+
+        [Test]
+        public void ShouldFail()
+        {
+            1.Should().Be(2);
+        }
+    }
+}
