86facb8012d6c1738527047818ba36e4aff88b97
Mateusz Tadla <mtadla@sii.pl>
2020-03-31 03:32:59 +0200

    update

---
+++ BambooTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BambooDemo
+{
+    [TestFixture]
+    class BambooTests
+    {
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.Pass();
+        }
+    }
+}
