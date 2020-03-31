f8044987f30bbb34fdea05f248cabec12e447d3c
Mateusz Tadla <mtadla@sii.pl>
2020-03-31 03:09:54 +0200

    update

--- UnitTests.cs
+++ UnitTests.cs
@@ -1,24 +1,21 @@
-﻿using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Allure.Core;
-using NUnit.Framework;
 
-namespace BambooDemo.Base
+namespace BambooDemo
 {
-    // [AllureNUnit]
     [TestClass]
-    public class DemoTests 
+    public class UnitTests
     {
         [TestMethod]
         public void ShouldPass()
         {
-            1.Should().Be(1);
+            Assert.AreEqual(10, 10);
         }
+
 
         [TestMethod]
         public void ShouldFail()
         {
-            1.Should().Be(2);
+            Assert.AreEqual(10, 11);
         }
     }
 }
